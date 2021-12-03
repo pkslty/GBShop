2115d43739c20a66e27e7518cf934451b38809d4
pkslty <pkslty@yandex.ru>
2021-12-03 16:02:45 +0300

    Add unit test from lecture

--- GBShopTests/GBShopTests.swift
+++ GBShopTests/GBShopTests.swift
@@ -6,28 +6,64 @@
 //
 
 import XCTest
+import Alamofire
 @testable import GBShop
 
 class GBShopTests: XCTestCase {
+    
+    let expectation = XCTestExpectation(description: "Download https://failUrl")
+    var errorParser: ErrorParserStub!
+    
+    struct PostStub: Codable {
+        let userId: Int
+        let id: Int
+        let title: String
+        let body: String
+    }
+
+    enum ApiErrorStub: Error {
+        case fatalError
+    }
+
+    struct ErrorParserStub: AbstractErrorParser {
+        func parse(_ result: Error) -> Error {
+            return ApiErrorStub.fatalError
+        }
+        
+        func parse(response: HTTPURLResponse?, data: Data?, error: Error?) -> Error? {
+            return error
+        }
+    }
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        errorParser = ErrorParserStub()
+
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        
+        errorParser = nil
+        
     }
+    
+    func testShouldDownloadAndParse() {
+        let errorParser = ErrorParserStub()
+        
+        AF.request("https://jsonplaceholder.typicode.com/posts/1").responseCodable(errorParser: errorParser) {(response: DataResponse<PostStub, AFError>) in
+            switch response.result {
+                case .success(_): break
+                case .failure: XCTFail()
+            }
+            self.expectation.fulfill()
+
+        }
+        wait(for: [expectation], timeout: 10.0)
+    }
+
 
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
     }
 
 }
