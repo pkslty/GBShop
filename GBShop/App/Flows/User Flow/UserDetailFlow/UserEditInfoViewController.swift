2aa2664011fafb2a913a24a69cfb8934feab25e4
pkslty <pkslty@yandex.ru>
2021-12-27 20:24:11 +0300

    Add Edit user info view controller

---
+++ GBShop/App/Flows/User Flow/UserDetailFlow/UserEditInfoViewController.swift
@@ -0,0 +1,17 @@
+//
+//  UserEditInfoViewController.swift
+//  GBShop
+//
+//  Created by Denis Kuzmin on 27.12.2021.
+//
+
+import UIKit
+
+class UserEditInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
