454690c270ed9ee9e132574a39e80c23f6fdb15e
pkslty <pkslty@yandex.ru>
2022-01-19 01:34:16 +0300

    Prepare reviev view

---
+++ GBShop/App/Flows/CatalogFlow/Review/ReviewListCell.swift
@@ -0,0 +1,23 @@
+//
+//  ReviewListCell.swift
+//  GBShop
+//
+//  Created by Denis Kuzmin on 19.01.2022.
+//
+
+import UIKit
+
+class ReviewListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
