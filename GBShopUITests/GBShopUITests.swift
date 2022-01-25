04be4819a3a27bf921d612cbf5a6392385cf5515
pkslty <pkslty@yandex.ru>
2022-01-26 02:27:50 +0300

    HomeWork 10

--- GBShopUITests/GBShopUITests.swift
+++ GBShopUITests/GBShopUITests.swift
@@ -8,29 +8,59 @@
 import XCTest
 
 class GBShopUITests: XCTestCase {
-
+    let app = XCUIApplication()
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+        app.launch()
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testLoginLogoutSuccess() throws {
+        app.tabBars["Tab Bar"].buttons["User"].tap()
+        let scrollViewsQuery = app.scrollViews
+        let elementsQuery = scrollViewsQuery.otherElements
+        let userNameField = elementsQuery.textFields["Username"]
+        userNameField.tap()
+        userNameField.typeText("12345678")
+        let passwordField = elementsQuery.secureTextFields["Password"]
+        passwordField.tap()
+        passwordField.typeText("12345")
+        
+        elementsQuery/*@START_MENU_TOKEN@*/.staticTexts["Sign In"]/*[[".buttons[\"Sign In\"].staticTexts[\"Sign In\"]",".staticTexts[\"Sign In\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        let nameField = scrollViewsQuery.otherElements.containing(.button, identifier:"Log Out").children(matching: .other).element.children(matching: .other).element.children(matching: .other).element(boundBy: 1).children(matching: .textField).element
+        XCTAssertNotNil(nameField.value)
+        let name = nameField.value as! String
+        XCTAssertEqual(name, "Jane Elisabeth Doe")
+        
+        let logoutButton = scrollViewsQuery.otherElements/*@START_MENU_TOKEN@*/.staticTexts["Log Out"]/*[[".buttons[\"Log Out\"].staticTexts[\"Log Out\"]",".staticTexts[\"Log Out\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        logoutButton.tap()
+        let authorizationLabel = scrollViewsQuery.otherElements.staticTexts["Authorization"]
+        XCTAssertNotNil(authorizationLabel.value)
+    
     }
-
+    
+    func testLoginFail () throws {
+        app.tabBars["Tab Bar"].buttons["User"].tap()
+        let elementsQuery = app.scrollViews.otherElements
+        let userNameField = elementsQuery.textFields["Username"]
+        userNameField.tap()
+        userNameField.typeText("12345678")
+        let passwordField = elementsQuery.secureTextFields["Password"]
+        passwordField.tap()
+        passwordField.typeText("123")
+        elementsQuery.buttons["Sign In"].tap()
+        let alert = app.alerts["Error"]
+        XCTAssertNotNil(alert)
+        alert.scrollViews.otherElements.buttons["Ok"].tap()
+                
+        
+    }
+    
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
             // This measures how long it takes to launch your application.
